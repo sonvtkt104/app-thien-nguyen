805550ccd037cbd0f9566a4de2052ab498bb0f8b
xuan son <nguyenxuanson.kt162@gmail.com>
2023-03-09 16:10:48 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import React  from 'react';
+
+function App() {
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div className="App">
+        hello du a hihio
+    </div>
+  );
+}
+
+export default App;
