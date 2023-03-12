69779e947410d6047d9cb6ba776f251788223d6d
haikhuat381 <haikhuat308@gmail.com>
2023-03-12 22:49:13 +0700

    commit 2

---
+++ src/pages/app/Donations/ModalDetail.jsx
@@ -0,0 +1,42 @@
+import { Button, Col, Input, Row, Table, Modal, Image   } from 'antd'
+import { useState } from 'react'
+
+
+function ModalDetail({ dataDetail, handleCloseModalDetail}) {
+    const [open,setOpen] = useState(true)
+    console.log(dataDetail)
+
+    return (
+        <Modal
+                title="Thông tin chi tiết"
+                cancelText = "Quay lại"
+                okText = "Liên hệ"
+                centered
+                width={700}
+                open={open}
+                onOk={()=> { setOpen(false); handleCloseModalDetail()}}
+                onCancel={()=> { setOpen(false); handleCloseModalDetail() }}
+                style={{fontFamily:"sans-serif"}}
+            >
+                <p><b>Người ủng hộ:</b> {dataDetail.donorName}</p>
+                <p><b>Thông tin liên hệ:</b> {dataDetail.contactInfo} </p>
+                <p><b>Thông tin ủng hộ:</b> {dataDetail.name}, ủng hộ {dataDetail.donationObject}, ở {dataDetail.donationAddress}</p>
+                <p><b>Mô tả:</b> {dataDetail.description}</p>
+                <p><b>Ảnh:</b></p>
+                <div>
+                    {
+                        dataDetail.images?.map((image,index) => 
+                                <Image 
+                                    style={{ paddingRight :"5px", paddingBottom :"5px"}}
+                                    key={index}
+                                    width={100}
+                                    src={image}>
+                                </Image>
+                            )
+                    }
+                </div>
+            </Modal>
+    )
+}
+
+export default ModalDetail
